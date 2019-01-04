8ab9f425c7f9de8c0a5eba9a93403973848e47d6
新真大 <ma.ly321.luna.notte@gmail.com>
2019-01-04 17:30:56 +0900

    Initial Commit

---
+++ QuestionBox2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuestionBox2
+//
+//  Created by 新真大 on 2019/01/04.
+//  Copyright © 2019 新真大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
