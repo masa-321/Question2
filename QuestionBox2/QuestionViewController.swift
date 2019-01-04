7d7d22d17d09e3825d1a29bee1238e894c1b3554
新真大 <ma.ly321.luna.notte@gmail.com>
2019-01-04 20:45:15 +0900

    viewcontrollerを追加。画像を追加。

---
+++ QuestionBox2/QuestionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuestionViewController.swift
+//  QuestionBox2
+//
+//  Created by 新真大 on 2019/01/04.
+//  Copyright © 2019 新真大. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
